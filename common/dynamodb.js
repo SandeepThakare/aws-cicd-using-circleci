62f8c0424c4f19003eef7be841c6aedbc6372d08
SandeepThakare <sandythakare9@gmail.com>
2018-05-28 16:35:51 +0530

    deployment change

--- common/dynamodb.js
+++ common/dynamodb.js
@@ -4,18 +4,14 @@ import { DynamoDB } from 'aws-sdk'; // eslint-disable-line import/no-extraneous-
 let options = {};
 console.log('Envoirment -----> ', process.env.IS_OFFLINE)
 // connect to local DB if running offline
-// if (process.env.IS_OFFLINE) {
-// 	console.log('Inside of offline')
-// 	options = {
-// 		region: process.env.AWS_REGION,
-// 		endpoint: 'http://localhost:8000',
-// 	};
-// }
+if (!process.env.IS_OFFLINE) {
+	console.log('Inside of offline')
+	options = {
+		region: us-east-1,
+		endpoint: 'http://localhost:8000',
+	};
+}
 
-options = {
-	region: 'us-east-1',
-	endpoint: 'http://localhost:8000',
-};
 console.log('Outside')
 const dynamoDB = new DynamoDB.DocumentClient(options);
 
