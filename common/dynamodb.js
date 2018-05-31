0c9af403b68769ea0e27ccbbd719f838c1b4d33a
SandeepThakare <sandythakare9@gmail.com>
2018-05-31 16:24:27 +0530

    dd

--- common/dynamodb.js
+++ common/dynamodb.js
@@ -1,18 +1,19 @@
-require("dotenv").config({ path: "../.env" /* path to your project root folder */ });
-import { DynamoDB } from 'aws-sdk'; // eslint-disable-line import/no-extraneous-dependencies
-// import AWS from 'aws-sdk';
+require('dotenv').config({ path: '../.env' /* path to your project root folder */ });
+// import { DynamoDB } from 'aws-sdk'; // eslint-disable-line import/no-extraneous-dependencies
+import AWS from 'aws-sdk';
+// const dynamoDB = new AWS.DynamoDB.DocumentClient();
 let options = {};
-console.log('Envoirment -----> ', process.env.IS_OFFLINE)
+console.log('Envoirment -----> ', process.env.IS_OFFLINE);
 // connect to local DB if running offline
 if (!process.env.IS_OFFLINE) {
-	console.log('Inside of offline')
+	console.log('Inside of offline');
 	options = {
 		region: 'us-east-1',
 		endpoint: 'http://localhost:8000',
 	};
 }
 
-console.log('Outside')
-const dynamoDB = new DynamoDB.DocumentClient(options);
+console.log('Outside');
+const dynamoDB = new AWS.DynamoDB.DocumentClient(options);
 
 export default dynamoDB;
