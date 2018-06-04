b0f074396301a1cd1e2151622ad7a00ff603cfaa
SandeepThakare <sandythakare9@gmail.com>
2018-06-04 17:45:44 +0530

    by function

--- customer/getCust.js
+++ customer/getCust.js
@@ -4,28 +4,45 @@ import dynamoDB from '../common/dynamodb';
 import StatusCode from '../common/statusCode';
 let statusCode = new StatusCode().getStatusCode();
 
-export function getCust(event, context, callback) {
+export async function getCust(event, context, callback) {
 	context.callbackWaitsForEmptyEventLoop = false;
 	// console.log('Context ---> ', context);
 	// console.log('callback --->', callback);
 	let a = new Common();
-	let scanParams = new Common().scanParams(process.env.CUSTOMER_INFO || 'customer-info');
+	let scanParams = await new Common().scanParams(process.env.CUSTOMER_INFO || 'customer-info');
 
-	return new Promise((resolve, reject) => {
-		console.log(scanParams);
-		dynamoDB.scan(scanParams, (err, data) => {
-			if (err) {
-				console.log('Unable to scan table. ERROR JSON: ', JSON.stringify(err, undefined, 2));
-				reject(err);
-			}
-			resolve('success');
-			console.log('Result - ', callback(null, data));
-			console.log(a.callbackHandler(statusCode.OK, data));
-		});
-	}).then((data) => {
-		callback(null, a.callbackHandler(statusCode.OK, data));
-	})
-		.catch((error) => {
-			a.callbackHandler(statusCode.BAD_REQUEST, error);
-		});
+	// return new Promise((resolve, reject) => {
+	// 	console.log(scanParams);
+	// 	dynamoDB.scan(scanParams, (err, data) => {
+	// 		if (err) {
+	// 			console.log('Unable to scan table. ERROR JSON: ', JSON.stringify(err, undefined, 2));
+	// 			reject(err);
+	// 		}
+	// 		resolve('success');
+	// 		console.log('Result - ', callback(null, data));
+	// 		console.log(a.callbackHandler(statusCode.OK, data));
+	// 	});
+	// }).then((data) => {
+	// 	console.log('Inside then ---> ', data);
+	// 	callback(null, a.callbackHandler(statusCode.OK, data));
+	// })
+	// 	.catch((error) => {
+	// 		console.log('Inside catch --->', error);
+	// 		a.callbackHandler(statusCode.BAD_REQUEST, error);
+	// 	});
+
+	dynamoDB.scan(scanParams, onScan);
+
+	async function onScan(err, data) {
+		if (err) {
+			console.log('Unable to scan table. ERROR JSON: ', JSON.stringify(err, undefined, 2));
+			callback(null, await a.callbackHandler(statusCode.BAD_REQUEST, err));
+			return;
+		}
+
+		console.log('Result - ', data);
+		console.log(a.callbackHandler(statusCode.OK, data));
+		callback(null, await a.callbackHandler(statusCode.OK, data));
+		return;
+	}
 }
