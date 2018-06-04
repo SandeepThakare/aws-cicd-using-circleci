006732aa1e76dbec0f5fd655c360afe9c5bea79a
SandeepThakare <sandythakare9@gmail.com>
2018-06-04 17:49:42 +0530

    webpack error solved

--- handler/handler.js
+++ handler/handler.js
@@ -3,15 +3,16 @@
 import Common from '../common/common';
 
 export const hello = async (event, context, callback) => {
-	const response = {
-		statusCode: 200,
-		body: JSON.stringify({
-			message: 'hello there',
-		}),
-	};
+	// const response = {
+	// 	statusCode: 200,
+	// 	body: JSON.stringify({
+	// 		message: 'hello there',
+	// 	}),
+	// };
 
 	console.log(await new Common().callbackHandler(200, 'abc'));
 
-	callback(null, new Common().callbackHandler(200, 'hello there	'));
+	callback(null, new Common().callbackHandler(200, 'hello there'));
+	return;
 };
 
