6c69e1260445af1cbddefe5ceb609d92e22de1e2
SandeepThakare <sandythakare9@gmail.com>
2019-01-09 17:28:19 +0530

    upload done

--- customer/fileUploadNewWay.js
+++ customer/fileUploadNewWay.js
@@ -6,91 +6,98 @@ import sha1 from 'sha1';
 import Common from '../common/common';
 import StatusCode from '../common/statusCode';
 import multipart from 'aws-lambda-multipart-parser';
+import MultipartLambda from 'lambda-multipart';
 let statusCode = new StatusCode().getStatusCode();
 let s3 = new S3();
 
 export function uploadImage(event, context, callback) {
 
-	// console.log('Event body=', JSON.stringify(event.body));
-	console.log('Event=', JSON.stringify(event));
-	console.log('Event=', event.headers);
-	console.log('Event body=', typeof (event.body));
-	let createdAt = new Date().toUTCString();
-	let etag = null;
+    // console.log('Event body=', JSON.stringify(event.body));
+    console.log('Event=', JSON.stringify(event));
+    console.log('Event=', event.headers);
+    console.log('Event body=', typeof (event.body));
+    let createdAt = new Date().toUTCString();
+    let etag = null;
 
-	let myBucket = 'aws-poc-image-bucket';
-	let url_prefix = 'https://s3.amazonaws.com';
+    let myBucket = 'aws-poc-image-bucket';
+    let url_prefix = 'https://s3.amazonaws.com';
 
-	if (!event.pathParameters || !event.pathParameters.email || !event.pathParameters.filename) {
+    if (!event.pathParameters || !event.pathParameters.email || !event.pathParameters.filename) {
 
-		callback(null, new Common().callbackHandler(statusCode.BAD_REQUEST, 'Email or File name is missing !'));
-		return;
-	}
+        callback(null, new Common().callbackHandler(statusCode.BAD_REQUEST, 'Email or File name is missing !'));
+        return;
+    }
 
-	let fileDetails = multipart.parse(event, true);
+    let fileDetails = multipart.parse(event, true);
+    let parser = new MultipartLambda(event);
 
-	// Begins the upload to the AWS S3
-	s3.headBucket({ Bucket: myBucket }, (err, data) => {
-		console.log(data);
-		if (err) {
-			s3.createBucket({ Bucket: myBucket }, (err, data) => {
-				console.log(data);
-				console.log('Inside create bucket');
-				if (err) {
-					console.log(err);
-					callback(null, new Common().callbackHandler(statusCode.BAD_REQUEST, err));
-					return;
-				}
+    console.log('New Parser -- ', JSON.stringify(parser))
 
-				let params = { 
-					Bucket: myBucket, 
-					Key: `${event.pathParameters.email}/${event.pathParameters.filename}`, 
-					Body: fileDetails.image_buffer.content, 
-					ContentEncoding: 'base64', 
-					ContentType: fileDetails.image_buffer.contentType, 
-					ACL: 'public-read' 
-				};
+    // Begins the upload to the AWS S3
+    s3.headBucket({ Bucket: myBucket }, (err, data) => {
+        console.log(data);
+        if (err) {
+            s3.createBucket({ Bucket: myBucket }, (err, data) => {
+                console.log(data);
+                console.log('Inside create bucket');
+                if (err) {
+                    console.log(err);
+                    callback(null, new Common().callbackHandler(statusCode.BAD_REQUEST, err));
+                    return;
+                }
 
-				s3.putObject(params, function (err, data) {
-					console.log(data);
-					if (err) {
-						console.log(JSON.stringify(err. undefined, 2));
-						callback(null, new Common().callbackHandler(statusCode.FORBIDDEN, err));
-						return;
-					} else {
-						console.log('Successfully uploaded data to myBucket/myKey');
+                let params = {
+                    Bucket: myBucket,
+                    Key: `${event.pathParameters.email}/${event.pathParameters.filename}`,
+                    Body: fileDetails.image_buffer.content,
+                    ContentEncoding: 'base64',
+                    ContentType: fileDetails.image_buffer.contentType,
+                    ACL: 'public-read'
+                };
+
+                s3.putObject(params, function (err, data) {
+                    console.log(data);
+                    if (err) {
+                        console.log(JSON.stringify(err.undefined, 2));
+                        callback(null, new Common().callbackHandler(statusCode.FORBIDDEN, err));
+                        return;
+                    } else {
+                        console.log('Successfully uploaded data to myBucket/myKey');
                         callback(null, new Common().callbackHandler(statusCode.OK, 'Image successfully saved !!!'));
                         return;
-					}
-				});
-			});
-		} else {
+                    }
+                });
+            });
+        } else {
             console.log('Inside create bucket else');
-            console.log(JSON.stringify(fileDetails));
+            // console.log(JSON.stringify(fileDetails));
+            let buff = new Buffer(parser.files[0]._readableState.buffer.head.data);
+            let base64data = buff.toString('base64');
+            console.log('Data --- ', JSON.stringify(base64data))
 
-			let params = { 
-				Bucket: myBucket, 
-				Key: `${event.pathParameters.email}/${fileDetails.image_buffer.filename}`, 
-				Body: fileDetails.image_buffer.content, 
-				ContentEncoding: 'base64', 
-				ContentType: fileDetails.image_buffer.contentType, 
-				ACL: 'public-read' 
-			};
+            let params = {
+                Bucket: myBucket,
+                Key: `${event.pathParameters.email}/${fileDetails.image_buffer.filename}`,
+                Body: fileDetails.image_buffer.content,
+                ContentEncoding: 'base64',
+                ContentType: fileDetails.image_buffer.contentType,
+                ACL: 'public-read'
+            };
 
-            console.log('params',JSON.stringify(params))
+            console.log('params', JSON.stringify(params))
 
-			s3.putObject(params, function (err, data) {
-				console.log(data);
-				if (err) {
-					console.log(err);
-					callback(new Common().callbackHandler(statusCode.FORBIDDEN, err));
-					return;
-				} else {
-					console.log('Successfully uploaded data to myBucket/myKey');
-					etag = data.ETag;
+            s3.putObject(params, function (err, data) {
+                console.log(data);
+                if (err) {
+                    console.log(err);
+                    callback(new Common().callbackHandler(statusCode.FORBIDDEN, err));
+                    return;
+                } else {
+                    console.log('Successfully uploaded data to myBucket/myKey');
+                    etag = data.ETag;
                     callback(null, new Common().callbackHandler(statusCode.OK, 'Image successfully saved !!!'));
-				}
-			});
-		}
-	});
+                }
+            });
+        }
+    });
 }
